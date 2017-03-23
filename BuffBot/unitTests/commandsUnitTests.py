524854ae296913865f7f2b34cb9d277fc7941c9f
Martin Nenseth <martin.nenseth@gmail.com>
2017-03-23 23:16:54 +0100

    ...

--- BuffBot/unitTests/commandsUnitTests.py
+++ BuffBot/unitTests/commandsUnitTests.py
@@ -10,7 +10,11 @@ class CommandsTest(unittest.TestCase):
     def test_bye(self):
         self.assertEqual(self.commands.bye("!bye", "85431603408420864"), "Bye bye!")
 
+    def test_math(self):
+        self.assertEqual(self.commands.math("!math", "85431603408420864", params=4 * 4), 16)
+
     #TODO: test all other commands when they've been refactored in commandsRefactored.
+
 
 if __name__ == '__main__':
     unittest.main(exit=False)
