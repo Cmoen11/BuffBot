3da96ad36566d877266c277736a2a2f073a11a5b
Christian Moen <moenc15@student.uia.no>
2017-03-27 12:13:02 +0200

    added database name into var for easy edit.

--- BuffBot/database.py
+++ BuffBot/database.py
@@ -4,10 +4,11 @@ import sqlite3
 class Database():
     def __init__(self):
         self.conn = None
+        self.DB_NAME = "test123.db"
         print("Opened database successfully")
 
     def flag_gaming_channel(self, channel_id, game_title, allowed):
-        self.conn = sqlite3.connect('test123.db')
+        self.conn = sqlite3.connect(self.DB_NAME)
         sql = 'insert into game_restriction (channel_ID, title, allowed) ' \
               'VALUES ("{}","{}",{})'.format(channel_id, game_title, allowed)
 
@@ -17,7 +18,7 @@ class Database():
 
     def get_flagged_games(self, channel_id):
         flagged_games = []
-        self.conn = sqlite3.connect('test123.db')
+        self.conn = sqlite3.connect(self.databaseName)
         games = self.conn.execute("select title from game_restriction where channel_ID = {} and allowed = 1"
                                   .format(channel_id))
 
@@ -29,7 +30,7 @@ class Database():
 
     @classmethod
     def set_coin_count_session_start(self, user_id, start_time, end_time, session_active):
-        self.conn = sqlite3.connect("test123.db")
+        self.conn = sqlite3.connect(self.DB_NAME)
         sql = 'insert into coins (user_id, start_time, end_time, session_active) ' \
               'VALUES ("{}",{},{})'.format(user_id, start_time, end_time, session_active)
 
@@ -39,7 +40,7 @@ class Database():
 
     @classmethod
     def set_coin_count_session_end(self, user_id, end_time, session_active):
-        self.conn = sqlite3.connect("test123.db")
+        self.conn = sqlite3.connect(self.DB_NAME)
         sql = 'UPDATE coins SET (end_time = {}, session_active = {}) ' \
               'WHERE user_id = "{}"'.format(end_time, session_active, user_id)
 
@@ -50,7 +51,7 @@ class Database():
     def get_coin_count(self, user_id):
         # TODO: Implement total coin value in DB as separate row
         total_coins = 0
-        self.conn = sqlite3.connect('test123.db')
+        self.conn = sqlite3.connect(self.DB_NAME)
         sessions = self.conn.execute("select start_time, end_time from coins where user_id = {} and session_active = 0"
                                   .format(user_id))
 
