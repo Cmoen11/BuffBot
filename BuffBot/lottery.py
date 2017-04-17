0930ad2e4c649c1921581df48db3faaafca27b9d
Erlend <erlend_thorvik@hotmail.com>
2017-04-18 01:24:48 +0200

    Math is hard

--- BuffBot/lottery.py
+++ BuffBot/lottery.py
@@ -15,7 +15,7 @@ class Lottery:
         self.winningTicket = 0
         self.ticketCounter = 0
         self.prizePool = 0
-        self.generate_winning_number(2)
+        self.generate_winning_number(100)
 
 
 
@@ -27,7 +27,7 @@ class Lottery:
         else:
             self.database.remove_coins(ctx.message.author.id, self.ticketCost)
             if self.ticketCounter == self.winningTicket:
-                self.generate_winning_number(2)
+                self.generate_winning_number(100)
                 self.database.insert_coins(user.id, self.prizePool, user.mention)
                 await self.bot.say("Congratulation, you won the lottery with a prizepool of " + str(self.prizePool),
                                    user.mention)
@@ -38,5 +38,5 @@ class Lottery:
     # Creates a new winning number, while resetting the counter, were the nr_of_tickets represents the odds of winning
     def generate_winning_number(self, nr_of_tickets):
         self.ticketCounter = 0
-        self.prizePool = nr_of_tickets * self.ticketCost
-        self.winningTicket = random.randrange(0, nr_of_tickets)
+        self.prizePool = nr_of_tickets * int(self.ticketCost / 2.2)
+        self.winningTicket = random.randrange(1, nr_of_tickets)
