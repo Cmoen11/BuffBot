b7e1e1e5cfc138a734f1ec1215b8a84050148202
Erlend <erlend_thorvik@hotmail.com>
2017-04-17 23:28:12 +0200

    Made lottery, which contains untested functionality of gambling

---
+++ BuffBot/lottery.py
@@ -0,0 +1,36 @@
+import random
+import discord
+from discord.ext import commands
+import database
+from coins import Coin
+
+class Lottery:
+    def __init__(self, bot):
+        self.bot = bot
+        self.database = database.Database(self.bot)
+        self.coins = Coin(bot)
+        # self.lotteryTickets = {} // Different implementation
+        self.ticketCost = 1
+        self.winningTicket = 0
+        self.ticketCounter = 0
+        self.generate_winning_number()
+
+
+
+    @commands.command(name="buyticket", pass_context=True)
+    async def buy_ticket(self, ctx):
+        user = ctx.message.author
+        if self.check_balance(user.id) < self.ticketCost:
+            await self.bot.say("You don't have enough coins", user.mention)
+        else:
+            self.database.remove_coins(ctx.message.author.id, self.ticketCost)
+            if self.ticketCounter == self.winningTicket:
+                self.generate_winning_number()
+                self.database.insert_coins(user.id, self.ticketCost * 10, user.mention)
+            else:
+                await self.bot.say("Better luck next time", user.mention)
+
+
+    def generate_winning_number(self, nr_of_tickets):
+        self.ticketCounter = 0
+        self.winningTicket = random.randrange(0, 10)
