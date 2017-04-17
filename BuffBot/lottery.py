2649b0bf8df676413e214a8fc5e5a9fd57f98053
Erlend <erlend_thorvik@hotmail.com>
2017-04-18 00:16:35 +0200

    Added a few missing parts

--- BuffBot/lottery.py
+++ BuffBot/lottery.py
@@ -1,6 +1,6 @@
 import random
-import discord
 from discord.ext import commands
+import discord
 import database
 from coins import Coin
 
@@ -13,6 +13,7 @@ class Lottery:
         self.ticketCost = 1
         self.winningTicket = 0
         self.ticketCounter = 0
+        self.prizePool = 0
         self.generate_winning_number(10)
 
 
@@ -21,15 +22,20 @@ class Lottery:
     async def buy_ticket(self, ctx):
         user = ctx.message.author
         if self.check_balance(user.id) < self.ticketCost:
-            await self.bot.say("You don't have enough coins", user.mention)
+            await self.bot.say("You don't have enough coins to buy a ticket", user.mention)
         else:
             self.database.remove_coins(ctx.message.author.id, self.ticketCost)
             if self.ticketCounter == self.winningTicket:
                 self.generate_winning_number(10)
-                self.database.insert_coins(user.id, self.ticketCost * 4, user.mention)
+                self.database.insert_coins(user.id, self.prizePool, user.mention)
+                await self.bot.say("Congratulation, you won the lottery with a prizepool of " + str(self.prizePool),
+                                   user.mention)
             else:
+                self.ticketCounter += 1
                 await self.bot.say("Better luck next time", user.mention)
-
+                
+    # Creates a new winning number, while resetting the counter, were the nr_of_tickets represents the odds of winning
     def generate_winning_number(self, nr_of_tickets):
         self.ticketCounter = 0
+        self.prizePool = nr_of_tickets * self.ticketCost
         self.winningTicket = random.randrange(0, nr_of_tickets)
