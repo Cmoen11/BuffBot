e54dda3bdee1900edf040d04a229f4a6f95ff9bd
Christian Moen <cmoen11@gmail.com>
2017-05-01 12:02:47 +0200

    better duration view

--- BuffBot/global_methods.py
+++ BuffBot/global_methods.py
@@ -45,10 +45,10 @@ async def send_message(channel, msg, bot):
 
 async def music_playing(player, bot, server) :
     embed = discord.Embed()
-
+    m, s = divmod(player.durationd, 60)
     embed.title = "Music"
     embed.add_field(name="Song name", value=player.title, inline=True)
-    embed.add_field(name="Duration", value=str(player.duration/60), inline=True)
+    embed.add_field(name="Duration", value=str("{}:{}".format(m, s)), inline=True)
     embed.add_field(name="Likes/dislike", value=str(player.likes)+"/"+str(player.dislikes), inline=True)
     embed.add_field(name="Views", value=str(player.views))
     embed.description = "Now playing.. "
