19e67f9d7e756c76a628ce543e5f6f8ddb0ce2c2
Martin Nenseth <martin.nenseth@gmail.com>
2017-05-03 18:49:23 +0200

    test_get_rich_users, test_poor_users_in_users, test_is_bot_in_rich_users

--- BuffBot/dbUnitTest.py
+++ BuffBot/dbUnitTest.py
@@ -6,28 +6,38 @@ from BuffBot.database import Database
 class DatabaseTest(unittest.TestCase):
     # Prepare the relevant table for testing by deleting all rows in it.
     def emptyTable(self, table):
-        self.conn = sqlite3.connect('test123.db')
+        self.conn = sqlite3.connect("test123.db")
         sql = 'delete from {} '.format(table)
         self.conn.execute(sql)
         self.conn.commit()
         self.conn.close()
 
     def setUp(self):
-        self.emptyTable("game_restriction")
-        self.emptyTable("main.members")
+        self.emptyTable('game_restriction')
+        self.emptyTable('members')
         # Create a Database object to call the relevant functions.
         self.db = Database()
         # Insert dummy data into the database.
         self.db.flag_gaming_channel("1", "Hearthstone", 1)
-        self.db.insert_coins(85431603408420864, 4000, mention=None)
-        self.db.insert_coins(235892294857916417, 23, mention=None)
-        self.db.insert_coins(278286021903515656, 20000, mention=None)  # Mocking the bot user.
+        self.db.insert_coins(85431603408420864, 4000, 'Lenny#1112')
+        self.db.insert_coins(235892294857916417, 23, 'Christian#1111')
+        self.db.insert_coins(278286021903515656, 20000, 'BuffBot#0334')  # Mocking the bot user.
 
     def test_flagged_games(self):
         self.assertEqual(self.db.get_flagged_games("1"), ["Hearthstone"])
 
     def test_get_rich_users(self):
-        self.assertEqual(self.db.get_rich_users(278286021903515656, 24), 0)
+        self.assertEqual([{'userid': 85431603408420864, 'coins': 4000, 'mention': 'Lenny#1112'}], self.db.get_rich_users(278286021903515656, 24))
+
+    def test_poor_users_in_rich_users(self):
+        self.assertNotIn([{'userid': 235892294857916417, 'coins': 23, 'mention': 'Christian#1111'}], self.db.get_rich_users(278286021903515656, 24))
+
+    def test_is_bot_in_rich_users(self):
+        self.assertNotIn([{'userid': 278286021903515656, 'coins': 20000, 'mention': 'BuffBot#0334'}],
+                         self.db.get_rich_users(278286021903515656, 24))
+
+
+
 
 
 if __name__ == '__main__':
