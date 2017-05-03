7ec95f62b4a6ff0504003d29bea2e42e75a64df8
Martin Nenseth <martin.nenseth@gmail.com>
2017-05-03 13:05:54 +0200

    unitTest get_rich_users

--- BuffBot/dbUnitTest.py
+++ BuffBot/dbUnitTest.py
@@ -14,15 +14,20 @@ class DatabaseTest(unittest.TestCase):
 
     def setUp(self):
         self.emptyTable("game_restriction")
-        # Create a Database object we can call functions on.
+        self.emptyTable("main.members")
+        # Create a Database object to call the relevant functions.
         self.db = Database()
         # Insert dummy data into the database.
         self.db.flag_gaming_channel("1", "Hearthstone", 1)
+        self.db.insert_coins(85431603408420864, 4000, mention=None)
+        self.db.insert_coins(235892294857916417, 23, mention=None)
+        self.db.insert_coins(278286021903515656, 20000, mention=None)  # Mocking the bot user.
 
     def test_flagged_games(self):
         self.assertEqual(self.db.get_flagged_games("1"), ["Hearthstone"])
 
-    #TODO: Test all coin functions when they are fully implemented
+    def test_get_rich_users(self):
+        self.assertEqual(self.db.get_rich_users(278286021903515656, 24), 0)
 
 
 if __name__ == '__main__':
